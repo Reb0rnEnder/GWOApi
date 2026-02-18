741ab20a461aa5a1cab7bb9186c33729fea9c459
RebornEnder <example@example.com>
2026-02-18 17:35:24 +0100

    fix shit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ readme = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name="gwo",
-    version="0.1.5",
+    version="0.1.6",
     packages=find_packages(),
     long_description=readme,
     long_description_content_type="text/markdown",
